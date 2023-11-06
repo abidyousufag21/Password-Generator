f09acd0e10f5dc2080503f438f74db0665833a48
Abid yousuf <abidyousufag21@gmail.com>
2023-11-06 20:19:54 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Password-Generator
+ password generate file
---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+
+
+var inputField = document.getElementById('password');
+var digit = 10;
+
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var symbol = "!@#$%^&*()_+}{|:?><|";
+
+
+var allChar = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    
+ 
+    var password = "" ;
+    password += upperCase[Math.floor(Math.random()* upperCase.length)];
+    password += lowerCase[Math.floor(Math.random()* lowerCase.length)];
+    password += number[Math.floor(Math.random()* number.length)];
+    password += symbol[Math.floor(Math.random()* symbol.length)];
+
+for(var i = 4; i < digit; i++){
+    password += allChar[Math.floor(Math.random()* allChar.length)];
+    
+}
+    inputField.value = password;    
+}
+
+function copyPassword(){
+    inputField.select();
+    document.execCommand("copy");
+}
+
+
+
